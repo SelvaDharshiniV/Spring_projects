02b656759ee2e8f9256938b3e8e9473d1e3bcb7c
Selvadharshini.V <Selvadharshini.V@EXIND3020-19.allfon.in>
2022-03-21 12:51:39 +0530

    new commit for changes in controller

--- jenkins-git-maven-docker/src/main/java/com/example/demo/controller/TeacherController.java
+++ jenkins-git-maven-docker/src/main/java/com/example/demo/controller/TeacherController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import java.util.*;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
